659a8702595e4f8f608809ee141331c25f073670
ポテト ^. .^₎ฅ <151442433+CheapNightbot@users.noreply.github.com>
2026-06-11 13:49:03 +0900

    Add transition to search/no result texts.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 import { Toaster } from "sonner";
 import Footer from "@/components/footer";
 import { MusicTable } from "@/components/music-table";
+import { cn } from "./lib/utils";
 
 function App() {
   const [loading, setLoading] = useState(false);
@@ -34,18 +35,23 @@ function App() {
         setLoading={setLoading}
         setResults={setResults}
       />
-      <ScrollArea className="border h-[clamp(600px,70vh,800px)] w-[clamp(600px,90vw,1200px)] rounded-sm">
+      <ScrollArea
+        className={cn(
+          "border h-[clamp(600px,70vh,800px)] w-[clamp(600px,90vw,1200px)] rounded-sm",
+          gotResults(results) && "rounded-tl-none",
+        )}
+      >
         {loading ? (
           <div className="h-[clamp(600px,70vh,700px)] w-full grid place-items-center">
-            <p className="text-xl text-muted-foreground animate-pulse">
-              Searching...𓏲 ๋࣭ ࣪ ˖🎐
+            <p className="text-xl text-muted-foreground animate-in fade-in-60 zoom-in-80 duration-500 ease-in-out">
+              <span className="animate-pulse">Searching...𓏲 ๋࣭ ࣪ ˖🎐</span>
             </p>
           </div>
         ) : gotResults(results) ? (
           <MusicTable musicList={results} />
         ) : (
           <div className="h-[clamp(600px,70vh,700px)] w-full grid place-items-center">
-            <p className="text-xl text-muted-foreground">
+            <p className="text-xl text-muted-foreground animate-in fade-in-60 zoom-in-80 duration-500 ease-in-out">
               Nothing to show here...⋆｡‧˚ʚ🧸ɞ˚‧｡⋆
             </p>
           </div>
