90ed8ea2e4ce78e39727803bf0dc4591bd3242f8
ポテト ^. .^₎ฅ <151442433+CheapNightbot@users.noreply.github.com>
2026-06-13 02:42:10 +0900

    Add settings button (does not work)

--- frontend/src/components/header/index.tsx
+++ frontend/src/components/header/index.tsx
@@ -1,15 +1,23 @@
+import { Button } from "@/components/ui/button";
+import { SettingsIcon } from "lucide-react";
 import "./header.css";
 
 function Header() {
   return (
-    <header className="text-center">
+    <header className="text-center flex items-center justify-center w-[clamp(400px,90vw,1200px)] py-4 mb-2">
       <h1
         id="header-logo"
-        className="p-4 select-none text-3xl font-semibold tracking-wide mb-2 hover:cursor-pointer"
+        className="flex-1 select-none text-3xl font-semibold tracking-wide hover:cursor-pointer"
         onClick={() => (window.location.href = "/")}
       >
         AriaBox ⨾<span className="rotate">💿</span>✮˚.⋆
       </h1>
+
+      <div className="">
+        <Button size="icon" variant="ghost">
+          <SettingsIcon className="size-1/2" />
+        </Button>
+      </div>
     </header>
   );
 }
