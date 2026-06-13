7b1fa9c655f6f4b97c6f857af2427d363d4a9d8e
ポテト ^. .^₎ฅ <151442433+CheapNightbot@users.noreply.github.com>
2026-06-13 20:00:48 +0900

    Create config.py

---
+++ backend/config.py
@@ -0,0 +1,23 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+backend_dir = os.path.abspath(os.path.dirname(__file__))
+project_root = os.path.dirname(backend_dir)
+
+
+class Config:
+    DATA_DIR = os.getenv("DATA_DIR", f"{project_root}/.app_data")
+    CONFIG_DIR = os.path.join(DATA_DIR, ".config")
+    SETTINGS_FILE = os.path.join(CONFIG_DIR, "settings.json")
+    SECRET_KEY_FILE = os.path.join(CONFIG_DIR, ".secret_key")
+    
+    DEFAULT_LANGUAGE = os.getenv("DEFAULT_LANGUAGE", "en")
+    DEFAULT_LOCATION = os.getenv("DEFAULT_LOCATION", "US")
+
+    HOST_URL = os.getenv("HOST_URL", "127.0.0.1")
+    HOST_PORT = int(os.getenv("HOST_PORT", 8960))
+
+    SECRET_KEY = os.getenv("SECRET_KEY")
