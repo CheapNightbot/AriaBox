66e2c3ffc6e162ca2f8f6f7d65debea199f04094
ポテト ^. .^₎ฅ <151442433+CheapNightbot@users.noreply.github.com>
2026-06-15 23:22:24 +0900

    Add music services logo/icons.

---
+++ frontend/src/assets/icons/index.ts
@@ -0,0 +1,5 @@
+import AppleMusicIcon from "@/assets/icons/AppleMusicIcon";
+import DeezerIcon from "@/assets/icons/DeezerIcon";
+import YouTubeMusicIcon from "@/assets/icons/YouTubeMusicIcon";
+
+export { AppleMusicIcon, DeezerIcon, YouTubeMusicIcon };
