abe6311918169ce935305a3f47f8ccb156d6d2ab
ポテト ^. .^₎ฅ <151442433+CheapNightbot@users.noreply.github.com>
2026-06-15 23:23:11 +0900

    Add button to go back to home if not on home page (i.e. "/").

--- frontend/src/components/header/index.tsx
+++ frontend/src/components/header/index.tsx
@@ -1,14 +1,27 @@
 import { Button } from "@/components/ui/button";
-import { SettingsIcon } from "lucide-react";
-import { NavLink } from "react-router";
+import { ArrowLeftIcon, SettingsIcon } from "lucide-react";
+import { NavLink, useLocation, useNavigate } from "react-router";
 import "./header.css";
 
 function Header() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
   return (
-    <header className="text-center flex items-center justify-center w-[clamp(400px,90vw,1200px)] py-4 mb-2">
+    <header className="text-center flex items-center justify-center w-[clamp(400px,90vw,1200px)] py-4 mb-2 relative">
+      {location.pathname !== "/" && (
+        <Button
+          size="sm"
+          className="absolute left-0 animate-in fade-in duration-500 ease-in-out slide-in-from-right-20"
+          onClick={() => void navigate("/")}
+        >
+          <ArrowLeftIcon /> Back Home
+        </Button>
+      )}
+
       <h1
         id="header-logo"
-        className="flex-1 select-none text-3xl font-semibold tracking-wide hover:cursor-pointer"
+        className="flex-1 select-none text-3xl font-semibold tracking-wide hover:cursor-pointer animate-in fade-in blur-in-sm duration-500 ease-in-out"
         onClick={() => (window.location.href = "/")}
       >
         AriaBox ⨾<span className="rotate">💿</span>✮˚.⋆
