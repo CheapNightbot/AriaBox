7687ed7cb40d010beff79068840e535db89c6a1e
ポテト ^. .^₎ฅ <151442433+CheapNightbot@users.noreply.github.com>
2026-06-23 03:14:54 +0900

    Add kaomoji (of course !!!!!!!!)

--- frontend/src/components/settings.tsx
+++ frontend/src/components/settings.tsx
@@ -224,7 +224,7 @@ function Settings() {
       ) : !settings ? (
         <div className="h-[clamp(600px,70vh,700px)] w-full grid place-items-center">
           <p className="text-xl text-muted-foreground animate-in fade-in-60 zoom-in-80 duration-500 ease-in-out">
-            Could not load settings.
+            Could not load settings. (⸝⸝๑﹏๑⸝⸝)
           </p>
         </div>
       ) : (
