c87c3c6344a28d3e8ee82cb205cb1a6f2881ef26
ポテト ^. .^₎ฅ <151442433+CheapNightbot@users.noreply.github.com>
2026-07-20 03:32:47 +0000

    Update logging date format

--- backend/app/logger.py
+++ backend/app/logger.py
@@ -7,7 +7,10 @@ logger.setLevel(logging.INFO)
 handler = logging.StreamHandler(sys.stdout)
 handler.setLevel(logging.INFO)
 
-formatter = logging.Formatter("[%(asctime)s] %(levelname)s in %(module)s: %(message)s")
+formatter = logging.Formatter(
+    "[%(asctime)s] %(levelname)-8s in %(module)s: %(message)s",
+    datefmt="%Y-%m-%d %I:%M:%S %p",
+)
 handler.setFormatter(formatter)
 
 if not logger.handlers:
